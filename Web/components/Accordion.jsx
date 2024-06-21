3d91976464eea7f6434e8c1c0dd6c1fde2f880db
RobinsonBeato <robinsonbeato95@gmail.com>
2024-06-21 01:29:22 -0300

    fixing accordion

---
+++ Web/components/Accordion.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+
+const AccordionItem = ({ title, content, isOpen, onClick }) => {
+  return (
+    <div className="accordion-item ">
+      <div className="accordion-title " onClick={onClick}>
+        <h3 className="h2 colored">{title}</h3>
+        <span className="accordion-icon open accordion-content"  >{isOpen ? <ion-icon style={{ color: 'var(--color-primary)'}} name="remove-outline"></ion-icon> : <ion-icon style={{ color: 'var(--color-primary)'}} name="add-outline"></ion-icon> }</span>
+      </div>
+      {isOpen && <div className="accordion-content section-text">{content}
+        <a href='#' className='link-accordion'>Ver más 
+           <ion-icon style={{ marginLeft: '10px'}} name="arrow-forward-outline"></ion-icon>
+        </a>
+        </div>}
+    </div>
+  );
+};
+
+const Accordion = ({ items }) => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  return (
+    <div className="accordion">
+      {items.map((item, index) => (
+        <AccordionItem
+          key={index}
+          title={item.title}
+          content={item.content}
+          isOpen={openIndex === index}
+          onClick={() => handleClick(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -10,11 +10,25 @@ import About from '../components/About'
 import WhatsApp from '../components/WhatsApp'
 import Contact from '../components/Contact'
 import ImageCarousel from '../components/ImageCarousel'
-
+import Accordion from '../components/Accordion'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const accordionItems = [
+    {
+      title: 'Título 1',
+      content: 'Este es el contenido para el primer ítem del acordeón.'
+    },
+    {
+      title: 'Título 2',
+      content: 'Este es el contenido para el segundo ítem del acordeón.'
+    },
+    {
+      title: 'Título 3',
+      content: 'Este es el contenido para el tercer ítem del acordeón.'
+    }
+  ];
 
   return (
     <>
@@ -22,6 +36,7 @@ function App() {
       <WhatsApp/>
       <MainSection />
       <Stats />
+      <Accordion items={accordionItems}/>
       <ImageCarousel/>
       <About/>
       <Contact/>
