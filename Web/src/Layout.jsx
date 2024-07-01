509bcab00a39b1998206edbb6a73fc0664c4533f
RobinsonBeato <robinsonbeato95@gmail.com>
2024-07-01 12:43:16 -0300

    adding WhatsApp to Layout

--- Web/src/Layout.jsx
+++ Web/src/Layout.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import WhatsApp from "../components/WhatsApp";
 import { Outlet, Link } from "react-router-dom";
 
 function Layout () {
     return (
       <>
        <Header/>
-     
+       <WhatsApp/>
         <Outlet />
            
         <Footer/>
